41083232afa87fd22a70b8c3462be93a23ebd0ee
root <wally666@wp.pl>
2017-01-02 00:22:46 +0100

    no message

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { AppRoutingModule }     from './app-routing.module';
 
@@ -12,6 +13,10 @@ import { MenuComponent } from './menu/menu.component';
 import { DocumentComponent } from './document/document.component';
 import { InvoiceComponent } from './invoice/invoice.component';
 import { FooterComponent } from './footer/footer.component';
+
+// import '@swimlane/ngx-datatable/release/datatable.css';
+// import '@swimlane/ngx-datatable/release/material.css';
+// import '@swimlane/ngx-datatable/release/assets/icons.css';
 
 @NgModule({
   declarations: [
@@ -23,6 +28,7 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent
   ],
   imports: [
+    NgxDatatableModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 
+// import '@swimlane/ngx-datatable/release/datatable.css';
+// import '@swimlane/ngx-datatable/release/material.css';
+// import '@swimlane/ngx-datatable/release/assets/icons.css';
+
 @Component({
   selector: 'app-invoice',
   templateUrl: './invoice.component.html',
-  styleUrls: ['./invoice.component.css']
+  styleUrls: [ './invoice.component.css' ]
 })
 export class InvoiceComponent implements OnInit {
+rows = [
+    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
+    { name: 'Dany', gender: 'Male', company: 'KFC' },
+    { name: 'Molly', gender: 'Female', company: 'Burger King' },
+  ];
+  // columns = [
+  //   { prop: 'name' },
+  //   { name: 'Gender' },
+  //   { name: 'Company' }
+  // ];
+  messages = {
+    // Message to show when array is presented
+    // but contains no values
+    emptyMessage: 'Brak danych do wyświetlenia',
+
+    // Footer total message
+    totalMessage: 'Razem'
+  };
+  count = this.rows.length;
+  editing = {};
 
   constructor() { }
 
   ngOnInit() {
   }
 
+updateValue(event, cell, cellValue, row) {
+    this.editing[row.$$index + '-' + cell] = false;
+    this.rows[row.$$index][cell] = event.target.value;
+  }
 }
